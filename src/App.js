14d40aee5dbb43f56a0d5f293197f7584564844f
matthcaptain <mathew.mercer914@gmail.com>
2023-10-19 04:17:51 -0400

    [update]

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Bulletproftlink Okta</h1>
+        <a href='https://trial-7146444.okta.com/'>Go to Okta</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
